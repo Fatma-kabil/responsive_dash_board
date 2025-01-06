b20def44fe66506b6699514f3234ee0a7476f0d0
Fatma_kabil <fatmakhaled112@gmail.com>
2025-01-06 13:37:55 +0200

    finish custom drawer

--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/utiles/app_images.dart';
+import 'package:responsive_dash_board/views/widgets/active_and_inactive_item.dart';
+import 'package:responsive_dash_board/views/widgets/drawer_item.dart';
 import 'package:responsive_dash_board/views/widgets/drawer_itetms_listview.dart';
 import 'package:responsive_dash_board/views/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child:const Column(
-        children: [
-           UserInfoListTile(
-              image: Assets.imagesAvatar3,
-              tile: 'Lekan Okeowo',
-              subtitle: 'demo@gmail.com'),
-           SizedBox(
-            height: 8,
-          ),
-          DrawerItemsLisView()
-        ],
-      ),
+      child: const Column(children: [
+        UserInfoListTile(
+            image: Assets.imagesAvatar3,
+            tile: 'Lekan Okeowo',
+            subtitle: 'demo@gmail.com'),
+        SizedBox(
+          height: 8,
+        ),
+        DrawerItemsLisView(),
+        Expanded(child: SizedBox()),
+        InActiveDrawerItem(
+            drawerItemModel: DrawerItemModel(
+                title: 'Setting system', image: Assets.imagesSettings)),
+        InActiveDrawerItem(
+            drawerItemModel: DrawerItemModel(
+                title: 'Logout account', image: Assets.imagesLogout)),
+      ]),
     );
   }
 }
