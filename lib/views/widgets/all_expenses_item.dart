ea48d5d2e6109e9dfea74352009dace9bec46bc3
Fatma_kabil <fatmakhaled112@gmail.com>
2025-01-08 11:42:31 +0200

    all expenses item part1

---
+++ lib/views/widgets/all_expenses_item.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/widgets/all_expenses_itemn_header.dart';
+
+class AllExpensesItem extends StatelessWidget {
+  const AllExpensesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+      decoration: ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+              side: const BorderSide(width: 1, color: Color(0xffF1F1F1)),
+              borderRadius: BorderRadius.circular(12))),
+      child: const Column(
+        children: [AllExpensesItemnHeader()],
+      ),
+    );
+  }
+}
---
+++ lib/views/widgets/all_expenses_itemn_header.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AllExpensesItemnHeader extends StatelessWidget {
+  const AllExpensesItemnHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 60,
+        height: 60,
+        decoration: const ShapeDecoration(
+            color: Color(0xffFAFAFA), shape: OvalBorder()),
+             child:  Row(children: [ ],),
+              );
+  }
+}
