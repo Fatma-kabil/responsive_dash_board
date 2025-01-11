31cdc19d251a3483fa2b45cf8d1c9a08d021f8c4
Fatma_kabil <fatmakhaled112@gmail.com>
2025-01-11 13:18:26 +0200

    create custom text field

---
+++ lib/views/widgets/custom_text_field.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+          fillColor: const Color(0xFFFAFAFA),
+          filled: true,
+          border: BuildBorder(),
+          enabledBorder: BuildBorder(),
+          focusedBorder: BuildBorder()),
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  OutlineInputBorder BuildBorder() {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: const BorderSide(color: Color(0xFFFAFAFA)));
+  }
+}
--- lib/views/widgets/quick_invoice.dart
+++ lib/views/widgets/quick_invoice.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/widgets/custom_background_container.dart';
+import 'package:responsive_dash_board/views/widgets/custom_text_field.dart';
 import 'package:responsive_dash_board/views/widgets/latest_transaction.dart';
 import 'package:responsive_dash_board/views/widgets/quick_invoice_header.dart';
 
@@ -13,7 +14,12 @@ class QuickInvoice extends StatelessWidget {
       child: Column(
         children: [
           QuickInvoiceHeader(),
-          LatestTransaction()
+          LatestTransaction(),
+          Divider(
+            height: 48,
+            color: Color(0xFFF1F1F1),
+          ),
+          CustomTextField()
         ],
       ),
     );
