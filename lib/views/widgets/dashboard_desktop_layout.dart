290b840fcf74642bcbe689e26cdd43b23424cb96
Fatma_kabil <fatmakhaled112@gmail.com>
2025-01-12 17:10:23 +0200

    create my cards page view part one

--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/views/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/views/widgets/my_card.dart';
+import 'package:responsive_dash_board/views/widgets/my_cards_page_view.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -18,7 +18,7 @@ class DashboardDesktopLayout extends StatelessWidget {
           SizedBox(
           width: 24,
         ),
-        Expanded(child: MyCard())
+        Expanded(child: MyCardsPageView())
       ],
     );
   }
