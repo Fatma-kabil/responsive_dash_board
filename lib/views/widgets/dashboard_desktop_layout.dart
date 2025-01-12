88f4482238585e36e931712a5d0bfa87573617a4
Fatma_kabil <fatmakhaled112@gmail.com>
2025-01-12 14:02:49 +0200

    create my card part two

--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/views/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/views/widgets/my_card.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -13,7 +14,8 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(
           width: 32,
         ),
-        Expanded(flex: 2, child: AllExpensesAndQuickInvoiceSection())
+        Expanded(flex: 2, child: AllExpensesAndQuickInvoiceSection()),
+        Expanded(child: MyCard())
       ],
     );
   }
