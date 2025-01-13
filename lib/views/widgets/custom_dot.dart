83d77bdeb914746e2adbc2659b80734dc6310b9e
Fatma_kabil <fatmakhaled112@gmail.com>
2025-01-13 13:05:05 +0200

    creat custom dot indicator

---
+++ lib/views/widgets/custom_dot.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomDotsIndicator extends StatelessWidget {
+  const CustomDotsIndicator({super.key, required this.isActive});
+  final bool isActive;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      height: isActive ? 32 : 8,
+      width: 8,
+      decoration: ShapeDecoration(
+          color: isActive ? const Color(0xff4EB7F2) : const Color(0xFFE7E7E7),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))),
+      duration: const Duration(milliseconds: 300),
+    );
+  }
+}
