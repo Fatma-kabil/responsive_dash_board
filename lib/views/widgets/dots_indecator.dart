88201df7f4d6a2dd784421ada50df3414df9d5bb
Fatma_kabil <fatmakhaled112@gmail.com>
2025-01-13 13:11:28 +0200

    create dots indicator

---
+++ lib/views/widgets/dots_indecator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/widgets/custom_dot.dart';
+
+class DotsIndecator extends StatelessWidget {
+  const DotsIndecator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: List.generate(3, (index) {
+    return  Padding(
+        padding: const EdgeInsets.only(right: 8),
+        child: CustomDotsIndicator(isActive: false,),
+      );
+    },),);
+  }
+}
