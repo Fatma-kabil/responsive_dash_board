41980c928439cc9b8a6b20c2a7696adaf6132aea
Fatma_kabil <fatmakhaled112@gmail.com>
2025-01-14 13:44:50 +0200

    dashboard desktop layout done

--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/views/widgets/custom_drawer.dart';
 import 'package:responsive_dash_board/views/widgets/income_section.dart';
-
+import 'package:responsive_dash_board/views/widgets/my_cards_and_trasction_history_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -19,7 +19,19 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(
           width: 24,
         ),
-        Expanded(child: IncomeSection())
+        Expanded(
+            child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            MyCardsAndTrasctionHistorySection(),
+            SizedBox(
+              height: 24,
+            ),
+            Expanded(child: IncomeSection())
+          ],
+        ))
       ],
     );
   }
