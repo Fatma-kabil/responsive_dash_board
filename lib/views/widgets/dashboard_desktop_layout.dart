a221f8325dea685d500dcb9aefdfcd76ed0b334f
Fatma_kabil <fatmakhaled112@gmail.com>
2025-01-14 10:24:27 +0200

    create income header

--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/views/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/views/widgets/my_cards_and_trasction_history_section.dart';
+import 'package:responsive_dash_board/views/widgets/income_section.dart';
+
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -18,7 +19,7 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(
           width: 24,
         ),
-        Expanded(child: MyCardsAndTrasctionHistorySection())
+        Expanded(child: IncomeSection())
       ],
     );
   }
---
+++ lib/views/widgets/income_section.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utiles/app_styles.dart';
+import 'package:responsive_dash_board/views/widgets/custom_background_container.dart';
+import 'package:responsive_dash_board/views/widgets/range_option.dart';
+
+class IncomeSection extends StatelessWidget {
+  const IncomeSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomBackgroundContainer(
+        child: Row(
+      children: [
+        Text(
+          'Income',
+          style: AppStyles.styleSemiBold20,
+        ),
+        Expanded(child: SizedBox()),
+        RangeOptions()
+      ],
+    ));
+  }
+}
