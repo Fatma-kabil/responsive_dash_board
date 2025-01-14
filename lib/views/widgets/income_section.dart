771a5902a7206b3b27780871b60ac354672300ea
Fatma_kabil <fatmakhaled112@gmail.com>
2025-01-14 12:04:39 +0200

    create income chart

--- lib/views/widgets/income_section.dart
+++ lib/views/widgets/income_section.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/utiles/app_styles.dart';
 import 'package:responsive_dash_board/views/widgets/custom_background_container.dart';
+import 'package:responsive_dash_board/views/widgets/income_chart.dart';
 import 'package:responsive_dash_board/views/widgets/range_option.dart';
 
 class IncomeSection extends StatelessWidget {
@@ -9,14 +10,23 @@ class IncomeSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const CustomBackgroundContainer(
-        child: Row(
+        child: Column(
       children: [
-        Text(
-          'Income',
-          style: AppStyles.styleSemiBold20,
+        Row(
+          children: [
+            Text(
+              'Income',
+              style: AppStyles.styleSemiBold20,
+            ),
+            Expanded(child: SizedBox()),
+            RangeOptions()
+          ],
         ),
-        Expanded(child: SizedBox()),
-        RangeOptions()
+        Expanded(
+          child: Row(
+            children: [Expanded(child: IncomeChart())],
+          ),
+        )
       ],
     ));
   }
