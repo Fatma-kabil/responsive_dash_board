e428212c1379afa7b13c106ba2a1cb6deb7a94d9
Fatma_kabil <fatmakhaled112@gmail.com>
2025-01-16 12:55:04 +0200

    create mnobile layout part two

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/widgets/adaptive_layout.dart';
+import 'package:responsive_dash_board/views/widgets/custom_drawer.dart';
 import 'package:responsive_dash_board/views/widgets/dashboard_desktop_layout.dart';
 import 'package:responsive_dash_board/views/widgets/dashboard_mobile_layout.dart';
 import 'package:responsive_dash_board/views/widgets/dashboard_tablet_layout.dart';
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
 
   @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  final GlobalKey<ScaffoldState> scaffoldkey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldkey,
       appBar: MediaQuery.sizeOf(context).width < 800
           ? AppBar(
               backgroundColor: const Color(0xff4EB7F2),
               elevation: 0,
-              leading: const Icon(
-                Icons.menu,
-                color: Colors.white,
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  scaffoldkey.currentState!.openDrawer();
+                },
               ),
             )
           : null,
+      drawer: MediaQuery.sizeOf(context).width < 800 ? const CustomDrawer() : null,
       backgroundColor: const Color(0XFFF7F9FA),
       body: AdaptiveLayout(
           mobilelayout: (context) => const DashboardMobileLayout(),
