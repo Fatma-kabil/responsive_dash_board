44b6f398555ee35d8113b61a2c1576a947f57e2b
Fatma_kabil <fatmakhaled112@gmail.com>
2025-01-18 10:22:06 +0200

    create detailed income chart

---
+++ lib/views/widgets/detailed_income_chart.dart
@@ -0,0 +1,53 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class DetailedIncomeChart extends StatefulWidget {
+  const DetailedIncomeChart({super.key});
+
+  @override
+  State<DetailedIncomeChart> createState() => _DetailedIncomeChartState();
+}
+
+class _DetailedIncomeChartState extends State<DetailedIncomeChart> {
+  int activeIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(aspectRatio: 1, child: PieChart(getChatData()));
+  }
+
+  PieChartData getChatData() {
+    return PieChartData(
+        pieTouchData: PieTouchData(
+          enabled: true,
+          touchCallback: (p0, PieTouchResponse) {
+            activeIndex =
+                PieTouchResponse?.touchedSection?.touchedSectionIndex ?? -1;
+            setState(() {});
+          },
+        ),
+        sectionsSpace: 0,
+        sections: [
+          PieChartSectionData(
+              value: 40,
+              color: const Color(0xFF208CC8),
+              showTitle: false,
+              radius: activeIndex == 0 ? 60 : 50),
+          PieChartSectionData(
+              value: 25,
+              color: const Color(0xFF4EB7F2),
+              showTitle: false,
+              radius: activeIndex == 1 ? 60 : 50),
+          PieChartSectionData(
+              value: 20,
+              color: const Color(0xFF064061),
+              showTitle: false,
+              radius: activeIndex == 2 ? 60 : 50),
+          PieChartSectionData(
+              value: 22,
+              color: const Color(0xFFE2DECD),
+              showTitle: false,
+              radius: activeIndex == 3 ? 60 : 50),
+        ]);
+  }
+}
