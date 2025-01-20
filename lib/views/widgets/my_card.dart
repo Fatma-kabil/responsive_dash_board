fc0c94adbec0e748dfd0ad0f34f05a2c3840f900
Fatma_kabil <fatmakhaled112@gmail.com>
2025-01-20 14:14:24 +0200

    responsive desktop layout done

--- lib/views/widgets/my_card.dart
+++ lib/views/widgets/my_card.dart
@@ -53,8 +53,10 @@ class MyCard extends StatelessWidget {
                 ],
               ),
             ),
-            const SizedBox(
-              height: 54 - 28,
+            const Flexible(
+              child:  SizedBox(
+                height: 54 - 28,
+              ),
             )
           ],
         ),
